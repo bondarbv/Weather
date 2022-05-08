cb8c95cb63d3a25af52747a2aee2e8049bfc22c5
bondarbv <92814470+bondarbv@users.noreply.github.com>
2022-05-08 21:13:13 +0300

    Created UI

--- Weather/ViewControllers/WeatherViewController.swift
+++ Weather/ViewControllers/WeatherViewController.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view = WeatherView()
     }
-
-
 }
 
